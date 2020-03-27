f493ac5f732ad354babfde68c74c222c9450fae1
Paul Vincent Craven <paul@cravenfamily.com>
2020-03-27 17:06:41 -0500

    Update

--- __main__.py
+++ __main__.py
@@ -3,6 +3,9 @@ import shutil
 import time
 import threading
 
+filter = [".git", ".hg"]
+
+
 class Stats:
     files_copied = 0
     files_skipped = 0
@@ -19,8 +22,13 @@ def thread_function(stats):
 
 
 def sync_directory(source_path, destination_path, stats):
-    source_path_entries = os.listdir(source_path)
-    destination_path_entries = os.listdir(destination_path)
+    try:
+        source_path_entries = os.listdir(source_path)
+        destination_path_entries = os.listdir(destination_path)
+    except Exception as e:
+        print(f"Unable to get directory listing for {source_path}.")
+        stats.file_errors += 1
+        return
 
     for directory_entry in source_path_entries:
         source_full_path = os.path.join(source_path, directory_entry)
@@ -59,21 +67,24 @@ def sync_directory(source_path, destination_path, stats):
 
         # --- Directories
         else:
-            source_time = os.path.getmtime(source_full_path)
+            if directory_entry not in filter:
+                source_time = os.path.getmtime(source_full_path)
 
-            if directory_entry not in destination_path_entries:
-                print(f"Directory does not exist {destination_full_path}")
-                os.mkdir(destination_full_path)
-                os.utime(destination_full_path, (source_time, source_time))
+                if directory_entry not in destination_path_entries:
+                    print(f"Directory does not exist {destination_full_path}")
+                    os.mkdir(destination_full_path)
+                    os.utime(destination_full_path, (source_time, source_time))
 
-            sync_directory(source_full_path, destination_full_path, stats)
+                sync_directory(source_full_path, destination_full_path, stats)
+            else:
+                print(f"Filtered {destination_full_path}")
 
 
 def main():
     # source_path = "G:/My Drive"
     # destination_path = "G:/backup/My Drive"
-    source_path = "G:/My Drive/Simpson"
-    destination_path = "D:/backup/My Drive/Simpson"
+    source_path = "G:/My Drive/"
+    destination_path = "D:/backup/My Drive/"
 
     # source_path = "test_source"
     # destination_path = "test_backup"
